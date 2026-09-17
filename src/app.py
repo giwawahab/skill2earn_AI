4d02bdd60fcbef8010ba06d1d79eda4ca0202836
giwawahab <giwawahabtomiwa2022@gmail.com>
2026-09-17 10:19:34 +0100

    Change the server to remote server

--- src/app.py
+++ src/app.py
@@ -40,4 +40,11 @@ if __name__ == "__main__":
         title="Skill2earn Idea 🤗",
         description="Find your way to the paying job that match your skill set",
         chatbot=gr.Chatbot(show_label=False),
-    ).launch(css=CSS, js=JS, theme=gr.themes.Base())
+    ).launch(
+    css=CSS,
+    js=JS,
+    theme=gr.themes.Base(),
+    server_name="0.0.0.0",
+    server_port=int(os.environ.get("PORT", 7860)),
+    share=False
+)
