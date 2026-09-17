7264a32a5c910063f9d7eadc2f5c9d16f97ce85c
giwawahab <giwawahabtomiwa2022@gmail.com>
2026-09-17 14:48:51 +0100

    testing4

--- src/app.py
+++ src/app.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     css=CSS,
     js=JS,
     theme=gr.themes.Base(),
-    server_name="127.0.0.1",
+    server_name="0.0.0.0",
     server_port=int(os.environ.get("PORT", 7860)),
     share=False
 )
